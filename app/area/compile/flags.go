6fb117e0586120ccc53bd997938b0e9c8bf4121d
thisisaaronland <thisisaaronland@localhost>
2025-01-12 13:06:22 -0800

    block out more area code

--- app/area/compile/flags.go
+++ app/area/compile/flags.go
@@ -6,13 +6,30 @@ import (
 	"os"
 
 	"github.com/sfomuseum/go-flags/flagset"
+	"github.com/sfomuseum/go-flags/multi"
 )
+
+var geohash string
+var parent_ids multi.MultiInt64
+var ancestor_ids multi.MultiInt64
+var mode string
+
+var external_source string
+var target string
 
 var verbose bool
 
 func DefaultFlagSet() *flag.FlagSet {
 
 	fs := flagset.NewFlagSet("flags")
+
+	fs.StringVar(&external_source, "external-source", "", "...")
+	fs.StringVar(&target, "target", "", "...")
+
+	fs.StringVar(&geohash, "geohash", "", "An optional geohash to do a prefix-first comparison against.")
+	fs.Var(&parent_ids, "parent-id", "One or more \"wof:parent_id\" values to match.")
+	fs.Var(&ancestor_ids, "ancestor-id", "Zero or more \"wof:hierarchies\" values to match.")
+	fs.StringVar(&mode, "mode", "all", "Indicate whether all or any filter criteria must match. Valid options are: any, all.")
 
 	fs.BoolVar(&verbose, "verbose", false, "Enable verbose (debug) logging.")
 
